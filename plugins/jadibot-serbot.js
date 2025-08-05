966b0690b1b364192844b8c52f15aa9681a6283e
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-08-05 10:54:06 -0400

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -183,7 +183,7 @@ fs.rmdirSync(pathMeguminJadiBot, { recursive: true })
 if (globalThis.db.data == null) loadDatabase()
 if (connection == `open`) {
 if (!globalThis.db.data?.users) loadDatabase()
-//await joinChannels(sock)
+await joinChannels(sock)
 const isCode = /^(qr|code)$/.test(command)
 if (m && conn && isCode && commandFlags[m.sender]) {
 await conn.sendMessage(m.chat, {text: `[✿] Listo, tu Sub-Bot ha quedado configurado.\n\n> *Visita:* https://api.stellarwa.xyz` }, { quoted: m })
@@ -209,7 +209,7 @@ delete globalThis.conns[i]
 globalThis.conns.splice(i, 1)
 }}, 60000)
 
-let handler = await import('../handler.js')
+let handler = await import('../megumin/handler.js')
 let creloadHandler = async function (restatConn) {
 try {
 const Handler = await import(`../handler.js?update=${Date.now()}`).catch(console.error)
@@ -323,7 +323,7 @@ async function checkSubBots() {
 
 setInterval(checkSubBots, 60000); //1min
 
-/*async function joinChannels(conn) {
+async function joinChannels(conn) {
 for (const channelId of Object.values(global.channel)) {
 await conn.newsletterFollow(channelId).catch(() => {})
-}}*/
+}}
