1af5c28195ce132938580f82e625268a30ac08ef
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-09-06 12:02:05 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -16,7 +16,21 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   let who = m.messageStubParameters[0] + '@s.whatsapp.net';
   let user = global.db.data.users[who];
-  let userName = user ? user.name : await conn.getName(who);
+  let userName;
+const nameFromDB = user && user.name;
+const isValidName = typeof nameFromDB === 'string'
+  && nameFromDB.trim()
+  && !(/undef|undefined|null|nan/i.test(nameFromDB));
+
+if (isValidName) {
+  userName = nameFromDB.trim();
+} else {
+  try {
+    userName = (await conn.getName(who)) || who.split('@')[0];
+  } catch (err) {
+    userName = who.split('@')[0];
+  }
+}
 
   let total = groupMetadata.participants.length;
 
